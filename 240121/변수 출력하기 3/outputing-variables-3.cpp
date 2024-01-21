98e989da34ee4ab7d21130204a9a2820242bfdee
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-21 15:04:03 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240121/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1 , b = 2;
+    char c = 'C';
+    
+    cout << a << "->" << b << "->" << c;    // 여기에 코드를 작성해주세요.
+    return 0;
+}
