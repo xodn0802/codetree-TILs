fb68f34ac25e1ea8458e96e6c5584fb376900c37
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-21 14:47:46 +0900

    Title: 정수 선언하고 곱 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240121/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a , b;
+    a = 26;
+    b = 5;
+    int c = a * b;
+    cout << a << " * " << b << " = " << c;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
