f2dfbb5e7e446beb4e9970555c6bd09b10f7cbac
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-21 11:56:18 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240121/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 97;
+    int b = 13;
+    int c = a - b;
+    cout << a << " - " << b <<  " = " << c;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
