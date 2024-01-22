b7f1fddb3426f8d4b7687b40cb003109065f53de
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-22 17:44:39 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/정수 복사/copy-integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3, b = 4;
+
+    b = a;
+
+    cout << a << " " << b << endl << a*b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
