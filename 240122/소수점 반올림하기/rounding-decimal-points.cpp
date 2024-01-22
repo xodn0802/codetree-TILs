bf05eea4f40f91ab2675ca6898dcf7c550ce68b0
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-22 17:15:17 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << fixed;
+    
+    double a = 25.352;
+    
+    cout.precision(1);
+    
+    cout << a;    // 여기에 코드를 작성해주세요.
+    return 0;
+}
