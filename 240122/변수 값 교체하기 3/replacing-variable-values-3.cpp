1b4910dec7f0ddc84d36e5333b29ea88f5491797
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-22 18:09:31 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3, b = 5, temp;
+    
+    temp = a;
+    a = b;
+    b = temp;
+    
+    cout << a << endl << b;    // 여기에 코드를 작성해주세요.
+    return 0;
+}
