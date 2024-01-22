3d113e35782292c2dbc4f006843ed94ae340cc36
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-22 21:25:41 +0900

    Title: 점수 입출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    
+    cin >> a;
+    
+    cout << "Your score is " << a << " point.";    // 여기에 코드를 작성해주세요.
+    return 0;
+}
