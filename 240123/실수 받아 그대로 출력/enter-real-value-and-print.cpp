7d93a682f3848e32b6542bd063ffb685da006a72
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-23 22:18:40 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<fixed;
+
+    double a;
+
+    cin >> a;
+
+    cout.precision(2);
+
+    cout << a;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
