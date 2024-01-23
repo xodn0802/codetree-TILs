3174b9d26e9a8d621b1a39e8ad29201c96ce2ba8
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-23 22:23:40 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+
+    double a;
+
+    cout.precision(2);
+
+    cin >> a;
+
+    cout << a + 1.5;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
