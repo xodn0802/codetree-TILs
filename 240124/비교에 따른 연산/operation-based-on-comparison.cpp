968553cf82bea23c826b115649b6ececc965fa08
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-24 21:32:46 +0900

    Title: 비교에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+    
+    if (a > b) {
+        cout << a * b;
+
+        return 0;
+
+    }   
+    
+    else {
+        cout << b / a;
+
+        return 0;
+    } // 여기에 코드를 작성해주세요.
+    return 0;
+}
