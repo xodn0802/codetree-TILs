cb54235fd7610ed462d631cb33f2352d9a0e0c2c
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-24 15:50:42 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    char c;
+    
+    cin >> m >> c >> d >> c >> y;
+    
+    cout << y << "." << m << "." << d;
+
+    return 0;
+}
