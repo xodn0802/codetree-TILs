eddf56463d104fd1f21f6c60759c87d4de804f28
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-24 21:30:33 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if (a >= 113) {
+        cout << "1";
+
+        return 0;
+    }
+
+    else {
+        cout << "0";
+
+        return 0;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
