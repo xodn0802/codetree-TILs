0e850a9348bcae160b54b59d82c758e08fd7b206
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-24 16:01:34 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, x, y;
+    char c;
+
+    cin >> a >> c >> x >> c >> y;
+
+    cout << "010" << c << y << c << x;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
