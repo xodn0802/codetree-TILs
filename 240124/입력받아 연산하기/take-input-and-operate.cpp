d5f0d401a00d9d5121fecdeae7e331681e872186
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-24 16:27:47 +0900

    Title: 입력받아 연산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    a +=87;
+    b %=10;
+
+    cout << a << endl << b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
