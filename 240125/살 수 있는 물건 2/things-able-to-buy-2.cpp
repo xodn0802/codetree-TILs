0e22695dfa26720bfd5d5e72b6a0405c94411da7
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 16:07:20 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin >> n;
+    
+    if (n >= 3000) {
+        cout << "book";
+        
+        return 0;
+    }
+    else if (n >= 1000) {
+        cout << "mask";
+
+        return 0;
+    }
+    else if (n >= 500) {
+        cout << "pen";
+        
+        return 0;
+    }
+
+    cout << "no";
+        // 여기에 코드를 작성해주세요.
+    return 0;
+}
