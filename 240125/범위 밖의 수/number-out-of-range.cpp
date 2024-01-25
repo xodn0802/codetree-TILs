555017c085a462ae0392ad44b0bb28d08dffabdf
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 19:24:32 +0900

    Title: 범위 밖의 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/범위 밖의 수/number-out-of-range.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    
+    if (a > 20 || a < 10) {
+        cout << "yes";
+
+    }
+    else {
+        cout << "no";
+
+    }
+      // 여기에 코드를 작성해주세요.
+    return 0;
+}
