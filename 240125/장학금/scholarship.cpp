6cf32850e7f13d6cd1452803c9f41c1a93db2889
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 19:40:30 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/장학금/scholarship.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    if (a >= 90 && b >= 95) {
+        cout << 100000;
+    }
+    else if (a >= 90 && b >= 90) {
+        cout << 50000;
+
+    }
+    else {
+        cout << 0;
+    }
+        // 여기에 코드를 작성해주세요.
+    return 0;
+}
