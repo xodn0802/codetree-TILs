853cf679cdd1bbf770cd9e7d64b8fe9634fd1c12
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 17:56:13 +0900

    Title: 4가지 관계연산자, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << (a >= b) << endl;
+    cout << (a > b) << endl;
+    cout << (b >= a) << endl;
+    cout << (b > a) << endl;
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
