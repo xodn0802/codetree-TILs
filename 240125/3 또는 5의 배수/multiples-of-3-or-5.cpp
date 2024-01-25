bde73cb4bc0551b7388a3900896471bd485b7d01
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 17:49:05 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if(a % 3 == 0) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    if(a % 5 == 0) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
