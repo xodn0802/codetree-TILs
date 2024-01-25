24d6ba6cef46950ba4d1c5798ac9875c6a47bdcf
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 18:11:51 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if (a >= 10 && a <= 20) {
+        cout << "yes";
+    }
+    else {
+        cout << "no";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
