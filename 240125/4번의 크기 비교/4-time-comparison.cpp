ca1bde98f15e89a262b92fbb8b46a43f0ad4eada
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 17:58:03 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << (a > b) << endl;
+    cout << (a > c) << endl;
+    cout << (a > d) << endl;
+    cout << (a > e) << endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
