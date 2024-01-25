ef552419a6d5f0f43e2d1bb08b4825df2f1a59d0
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 17:54:44 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/비교 연산/comparison-operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << (a >= b) << endl;
+    cout << (a > b) << endl;
+    cout << (b >= a) << endl;
+    cout << (b > a) << endl;
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
