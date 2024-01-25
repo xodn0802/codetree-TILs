7f7ebe8efbf3b5075bf585dbda06793fdfccf4ea
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 23:26:17 +0900

    Title: 최댓값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/최댓값 구하기/maximum-value.cpp
+++ 240125/최댓값 구하기/maximum-value.cpp
@@ -7,12 +7,15 @@ int main() {
 
     if (a >= b && a >= c) {
         cout << a;
+        return 0;
     }
     if (b >= a && b >= c) {
         cout << b;
+        return 0;
     }
     if (c >= a && c >= b){
         cout << c;
+        return 0;
     }
     // 여기에 코드를 작성해주세요.
     return 0;
