2681b38834f793af63e377df38af28c2cee5458a
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 16:20:03 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if (a == 5){
+        cout << 'A';
+        
+        return 0;
+        
+    }
+    if (a % 2 == 0){
+        cout << 'B';
+
+        return 0;
+    }
+        
+    
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
