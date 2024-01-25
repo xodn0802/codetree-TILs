be01020a6598e21bd30a3fe6b1381a1df97f0f2e
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 20:30:31 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/윤년인가/is-leap-year.cpp
+++ 240125/윤년인가/is-leap-year.cpp
@@ -4,21 +4,27 @@ using namespace std;
 int main() {
     int y;
     cin >> y;
-    
-    if (y % 4 == 0){
-        if(y % 100 == 0){
-            cout << "false";
-        }
-        else if(y % 100 == 0 && y % 400 == 0){
+
+    if (y % 4 == 0) {
+        if (y % 100 == 0 && y % 400 == 0){
             cout << "true";
+        }
+        else if (y % 100 ==0 && y % 400 !=0){
+            cout << "false";
         }
         else {
             cout << "true";
         }
+        
+        
+
 
     }
     else {
         cout << "false";
-    }    // 여기에 코드를 작성해주세요.
+    }
+    
+    
+    
     return 0;
 }
