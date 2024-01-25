836b57ea5dc3781039b19efc96e8f74e2e74acc8
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 15:52:17 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    
+    cin >> a;
+    
+    if (a == 1) {
+        cout << "John";
+        
+        return 0;
+    }
+    else if (a == 2) {
+        cout << "Tom";
+
+        return 0;
+    }  
+    else if ( a == 3) {
+        cout << "Paul";
+
+        return 0;
+    }
+
+    cout << "Vacancy";
+
+    
+      // 여기에 코드를 작성해주세요.
+    return 0;
+}
