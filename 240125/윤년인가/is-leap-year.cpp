9facc15c19a487322032ad2c56ab318c323db2ab
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-25 20:18:58 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240125/윤년인가/is-leap-year.cpp
+++ 240125/윤년인가/is-leap-year.cpp
@@ -3,23 +3,22 @@ using namespace std;
 
 int main() {
     int y;
+    cin >> y;
     
     if (y % 4 == 0){
-        if (y % 100 == 0){
-
-            if (y % 400 == 0){
-                cout << "true";
-
-                return 0;
-            }
-            else {
-                cout << "false";
-
-                return 0;
-            }
-
+        if(y % 100 == 0){
+            cout << "false";
         }
+        else if(y % 100 == 0 && y % 400 == 0){
+            cout << "true";
+        }
+        else {
+            cout << "true";
+        }
+
     }
-        // 여기에 코드를 작성해주세요.
+    else {
+        cout << "false";
+    }    // 여기에 코드를 작성해주세요.
     return 0;
 }
