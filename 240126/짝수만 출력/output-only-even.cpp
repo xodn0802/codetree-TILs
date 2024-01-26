057b1924036945087fc5d73cf61b106e533079f3
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 20:59:28 +0900

    Title: 짝수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, i;
+
+    cin >> a >> b;
+
+    i = a;
+
+    while(i <= b) {
+        cout << i << " ";
+        i += 2;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
