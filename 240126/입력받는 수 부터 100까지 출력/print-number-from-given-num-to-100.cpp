16d41d9b8bf50a9c756b5568bf17b29fbc833380
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 15:19:35 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int a = n; a <= 100; a ++){
+        cout << a << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
