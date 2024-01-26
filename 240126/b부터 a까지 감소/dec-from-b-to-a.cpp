fcfb0f6a3c3d0301482a3fdc44ed95f8390dfee1
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 20:26:56 +0900

    Title: b부터 a까지 감소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/b부터 a까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+    
+    for(int i = b; i >= a; i--){
+        cout << i << " ";
+    }    // 여기에 코드를 작성해주세요.
+    return 0;
+}
