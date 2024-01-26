05d00f3f8c2004796ad8ef3fc6d81cfab9ec611a
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 21:00:48 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 26;
+
+    while(a >= 10) {
+        cout << a << " ";
+        a--;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
