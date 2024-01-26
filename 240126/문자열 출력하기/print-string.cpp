f52ca401973d9bb9b57550b15c4a130531c56bcf
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 21:08:14 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        cout << "LeebrosCode" << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
