57a6a0464d6cc0c151dcdea8cff6239a7c9dbff3
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 16:55:27 +0900

    Title: 홀수만 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240126/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, x, i;
+
+    cin >> a >> b;
+    
+
+    x = 2i + 1;
+
+    for (int i = 0; a <= x && x <= b;  i++){
+        cout << 2i + 1 << " ";
+        
+
+    }
+        // 여기에 코드를 작성해주세요.
+    return 0;
+}
