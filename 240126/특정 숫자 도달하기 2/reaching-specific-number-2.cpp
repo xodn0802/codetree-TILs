ff09b6d7bfc3a783c7c99c6a9bda66adab9cec80
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 20:24:42 +0900

    Title: 특정 숫자 도달하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin >> n;
+    
+    for(int a = n; a >= 1; a--){
+        cout << a << " ";
+    }    // 여기에 코드를 작성해주세요.
+    return 0;
+}
