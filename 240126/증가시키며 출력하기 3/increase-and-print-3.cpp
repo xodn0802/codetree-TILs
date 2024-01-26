9164dace8892c839dbf4c0b63b2cde7b0866691d
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 20:54:39 +0900

    Title: 증가시키며 출력하기 3, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+
+    while (a <= 26) {
+        cout << a << " ";
+        a += 2;
+    }
+        // 여기에 코드를 작성해주세요.
+    return 0;
+}
