cd789f148e722e71f7d4afc70d6db5e67213b99d
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-26 20:45:25 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a;
+    cin >> n;
+    
+    a = 1;
+
+        while(a <= n) {
+            cout << a << " ";
+            a++;
+        }
+        // 여기에 코드를 작성해주세요.
+    return 0;
+}
