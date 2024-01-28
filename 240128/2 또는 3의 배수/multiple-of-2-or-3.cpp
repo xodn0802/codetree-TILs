6ce27eed6ead54ff53154912741972dc9d68e920
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-28 15:56:52 +0900

    Title: 2 또는 3의 배수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240128/2 또는 3의 배수/multiple-of-2-or-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace  std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 == 0 || i % 3 == 0){
+            cout << 1 << " ";
+        }
+        else {
+            cout << 0 << " ";
+        }
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
