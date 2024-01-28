7937fca7c42a6bec774fc0910ff06321fc529ffc
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-28 16:16:05 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240128/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    
+    cout << a << " ";
+    
+
+    for (int i = a; i < b;) {
+        if(i % 2 == 0) {
+            i += 3;
+            cout << i << " ";
+        }
+        else if(i % 2 == 1) {
+            i *= 2;
+            cout << i << " ";
+        }
+
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
