8050b70975c05d1fd50f8022d96daaec7b3c707b
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-29 20:44:48 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while(1) {
+        cin >> n;
+        
+        if(n == 0) {
+            break;
+        }
+        cout << n << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
