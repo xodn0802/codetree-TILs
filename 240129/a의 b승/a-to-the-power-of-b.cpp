35a701a4821b02e8c26880d42c20118005b542b6
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-29 15:52:58 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240129/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int prod = 1;
+    cin >> a >> b;
+    if(b > 0) {
+        for(int i = 1; i <= b; i++) {
+            prod *= a;
+        }
+    }
+    else if(b == 0){
+        cout << 1;
+    }
+    cout << prod;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
