26061486df43e8cab5d2d006b3e3fbd1f5df685e
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-30 16:54:05 +0900

    Title: 별표 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/별표 출력하기 2/print-star-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = n; i >= 1; i--) {
+        for(int j = 1; j <= i; j++) {
+            cout << "* ";
+
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
