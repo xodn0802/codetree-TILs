6064af094ae1274acccb8a88a05427d21e946958
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-30 18:15:54 +0900

    Title: 직각 삼각형 출력, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < 2 * i + 1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
