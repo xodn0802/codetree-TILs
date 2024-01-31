372222eaad777e2d7d3abb002a023b3fce049f5d
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-31 13:36:43 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i; j++) {
+            cout << "*";
+         }
+        for(int j = 0; j < i; j++){
+            cout << "  ";
+        }
+        for(int j = 0; j < n - i; j++) {
+            cout << "*";
+        }                
+        cout << endl;
+    }
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
