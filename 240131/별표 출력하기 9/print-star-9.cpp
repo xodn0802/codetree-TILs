9f21fb3137ce6ae6a363010a3f5aca576f7e9fa7
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-01-31 13:15:22 +0900

    Title: 별표 출력하기 9, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/별표 출력하기 9/print-star-9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++) {
+            cout << "  ";
+        }
+        for(int j = 1; j <= 2 * i - 1; j++) {
+            cout << "* ";
+
+        }
+        cout << endl;
+    }
+        // 여기에 코드를 작성해주세요.
+    return 0;
+}
