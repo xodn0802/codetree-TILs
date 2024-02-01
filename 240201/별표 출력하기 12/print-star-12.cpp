80cd93f750fbd167d648c2f4f875e4b763af4ad3
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-01 14:53:28 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/별표 출력하기 12/print-star-12.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++){
+            if(j % 2 == 0){
+                if(i == 0){
+                    cout << "* ";
+                }
+                else {
+                    cout << "  ";
+                }
+            }
+            else {
+                if (i <= j){
+                    cout << "* ";
+                }
+                else {
+                    cout << "  ";
+                }
+            }
+
+
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
