ce68d1063283d6fc8745dc56075041362d0e7850
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-01 22:08:46 +0900

    Title: 정상적인 구구단, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cout << i << " * " << j << " = " << i * j;
+            if(j < n){
+                cout << ", ";
+            }
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
