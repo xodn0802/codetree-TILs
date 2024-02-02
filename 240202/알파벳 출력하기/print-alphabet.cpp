e18e28cd2193663b17bf7b537f0f5ee3efcc3699
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-02 15:36:20 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240202/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    char cnt = 'A';
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i + 1; j++){
+            cout << cnt;
+            cnt++;
+            if(cnt == 'Z'){
+                cnt = 'A';
+            }
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
