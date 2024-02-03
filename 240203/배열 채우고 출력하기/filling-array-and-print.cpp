52d6c88f8841bde0f5112590451352bdb2ade95c
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-03 15:48:53 +0900

    Title: 배열 채우고 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240203/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char arr[10];
+
+    for(int i = 0; i < 10; i++){
+        cin >> arr[i];
+
+    }
+    for(int i = 9; i>= 0; i--){
+        cout << arr[i];
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
