b8c10839543e823dd47374bbaf96b394980f8d38
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-04 12:06:52 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240204/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char arr[10];
+    for(int i = 0; i < 10; i++){
+        cin >> arr[i];
+    }
+    cout << arr[1] << " " << arr[4] << " " << arr[7];
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
