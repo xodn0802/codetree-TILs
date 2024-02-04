68f4e6ce7b1415cc8574251a3b3d4b78be5aebd7
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-04 14:23:50 +0900

    Title: 배열에서 원소의 특징, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240204/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
+++ 240204/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -4,11 +4,13 @@ int main() {
     int arr[10];
     for(int i = 0; i < 10; i++){
         cin >> arr[i];
-        if(arr[i] == 3){
+        if(arr[i] % 3 == 0){
             cout << arr[i - 1];
             break;
         }
+
     }
+
     // 여기에 코드를 작성해주세요.
     return 0;
 }
