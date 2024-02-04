1f0dcd1379a2b115492618d88c8aa10ad67315b1
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-04 11:51:35 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240204/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    for(int i = 0; i < 10; i++){
+        cin >> arr[i];
+
+
+    }
+    cout << arr[2] + arr[4] + arr[9];
+        // 여기에 코드를 작성해주세요.
+    return 0;
+}
