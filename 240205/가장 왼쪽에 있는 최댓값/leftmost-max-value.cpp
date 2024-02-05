540e22e32b5720035bafc17d6abe3a74eaeeb6ff
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-05 23:32:16 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240205/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[1000];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int max_val = -1;
+    int m = 0;
+    int end;
+    end = n;
+    while(1) {
+        for(int i = 0; i < end; i++){
+            if(max_val < arr[i]){
+                max_val = arr[i];
+                m = i;
+                
+            }
+
+        }
+        cout << m + 1;
+        end = m;
+        max_val = -1;
+        if (m==0){
+            break;
+        }
+    }
+
+
+    
+        
+
+    
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
