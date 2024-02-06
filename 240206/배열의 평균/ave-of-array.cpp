a7b51ecb48d57ef48b01b84827dae37306f74255
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-06 14:57:34 +0900

    Title: 배열의 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240206/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[2][4];
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    
+    for(int i = 0; i < 2; i++){
+        cout << fixed;
+        cout.precision(1);
+        int sum = 0;
+        double av = 0;
+
+        for(int j = 0; j < 4; j++){
+            sum += arr[i][j]; 
+        }
+        av = sum / 4;
+        cout << av << " ";
+
+    }
+    cout << endl;
+    for(int j = 0; j < 4; j++){
+        cout << fixed;
+        cout.precision(1);
+        int sum = 0;
+        double av = 0;
+
+        for(int i = 0; i < 2; i++){
+            sum += arr[i][j];
+        }
+        av = sum / 2;
+        cout << av << " ";
+    }
+    cout << endl;
+    
+    cout << fixed;
+    cout.precision(1);
+    int sum = 0;
+    double av = 0;
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            sum += arr[i][j];
+        }
+    }
+    av = sum / 8;
+    cout << av;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
