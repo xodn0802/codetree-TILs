e8e2d40295446dd4832536214f1b5f0bc47d4415
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-07 14:37:07 +0900

    Title: 배열로 사각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/배열로 사각형 만들기/print-array-in-rectangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5][5];
+    for(int i = 0; i < 5; i++){
+        arr[0][i] = 1;
+    }
+    for(int i = 1; i < 5; i++){
+        arr[i][0] = 1;
+    }
+    for(int i= 1; i < 5; i++){
+        for(int j = 1; j < 5; j++){
+            arr[i][j] = arr[i][j - 1] +arr[i - 1][j];
+        }
+    }
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
