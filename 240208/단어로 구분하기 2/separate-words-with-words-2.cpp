e06cc6b31911e876d30b1a616022e98697ec8353
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-08 14:26:40 +0900

    Title: 단어로 구분하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string arr[10];
+    for(int i = 0; i < 10; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < 10; i += 2){
+        cout << arr[i] << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
