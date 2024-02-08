8d8b1a9a66d39695161a892dad663085e9b1101d
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-08 17:20:57 +0900

    Title: 문자열 범위 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240208/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240208/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -10,11 +10,16 @@ int main() {
     if(arr.length() < n){
         for(int i = arr.length() - 1; i >= 0; i--){
             cout << arr[i];
+            if(i == 0){
+                break;
+            }
         }
     }
     else {
         for(int i = arr.length() - 1; i >= arr.length() - n; i--){
             cout << arr[i];
+            if(i == arr.length() - n)
+                break;
         }
     }
         // 여기에 코드를 작성해주세요.
