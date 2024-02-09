9a7c4467184b68aa66f19dde3f5178daf54a01fc
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-09 12:41:41 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240209/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string a;
+    cin >> a;
+    string b = "Hello";
+    cout << a + b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
