39706bf9144b4ce46e85ba27814529fb637ee6e8
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-11 15:41:16 +0900

    Title: 문자 교체하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240211/문자 교체하기/changing-char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    b[0] = a[0];
+    b[1] = a[1];
+
+    cout << b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
