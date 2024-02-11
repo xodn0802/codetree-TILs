09a6d4f32295eb0c10908f058a6a143f35fed229
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-11 20:12:33 +0900

    Title: 첫 번째와 두 번째 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240211/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
+++ 240211/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -7,23 +7,17 @@ int main() {
     cin >> s;
 
     int n = s.length();
-    
 
-    for(int i = 2; i < n; i++){
-        if(s[i] == s[0]){
-            s[i] = s[1];
-        }
-        else if(s[i] == s[1]){
-            s[i] = s[1];
-        }
-        
-
-    }
     char a = s[0];
-    s[0] = s[1];
-    s[1] = a;
-    
+    char b = s[1];
+
+    for(int i = 0; i < n; i++){
+        if(s[i] == a) s[i] = b;
+        else if(s[i] == b) s[i] = a;
+    }
     cout << s;
+    
+
 
         // 여기에 코드를 작성해주세요.
     return 0;
