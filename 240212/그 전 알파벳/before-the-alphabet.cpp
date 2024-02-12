a47546a7d2067f04a8d923586f9b1298b15e2cb3
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-12 22:59:30 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240212/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char a;
+    cin >> a;
+    int b = (int)a - 1;
+    if(a == 'a'){
+        cout << 'z';
+    }
+    cout << (char)b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
