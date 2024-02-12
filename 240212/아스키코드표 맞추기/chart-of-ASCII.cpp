1b79420b92153736f3547421a837dca205c00a3d
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-12 22:40:41 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    cout << (char)a << " " << (char)b << " " << (char)c << " " << (char)d << " " << (char)e;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
