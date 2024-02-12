92fe2680c2f05d34601315178749535a2a82de67
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2024-02-12 22:42:06 +0900

    Title: 아스키코드 변환, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    int b;
+    cin >> a >> b;
+    cout << (int)a << " " << (char)b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
