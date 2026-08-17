48d2d129085834ef64ad175eafac025f80dddd6a
xodn0802 <96473118+xodn0802@users.noreply.github.com>
2026-08-17 11:32:06 +0900

    [Passed] 체크판위에서 2 (2ms, 0MB)

---
+++ trail2/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int R, C;
+char grid[15][15];
+
+int main() {
+    cin >> R >> C;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int cnt = 0;
+    for(int i = 1; i < R; i++){
+        for(int j = 1; j < C; j++){
+            for(int k = i + 1; k < R - 1; k++){
+                for(int l = j + 1; l < C - 1; l++){
+                    if(grid[0][0] != grid[i][j] && grid[i][j] != grid[k][l] && grid[k][l] != grid[R-1][C-1]) cnt++;
+
+                }
+            }
+        }
+    }
+    cout << cnt;
+
+    // Please write your code here.
+
+    return 0;
+}
